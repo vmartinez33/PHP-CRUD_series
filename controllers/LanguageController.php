e0d68e6e4ded4bc0860b8b54410fca481aeadbab
vmartinez33 <vmartineza33@gmail.com>
2022-07-06 16:53:01 +0200

    Se ha creado el controlador para los idiomas, con sus funciones asociadas.

---
+++ controllers/LanguageController.php
@@ -0,0 +1,92 @@
+<?php
+    require_once('../../models/Language.php');
+
+    function initConnectionDb() {
+        $db_host = 'localhost';
+        $db_user = 'root';
+        $db_password = 'root';
+        $db_db = 'bibliotecaseries';
+
+        $mysqli = @new mysqli(
+            $db_host,
+            $db_user,
+            $db_password,
+            $db_db
+        );
+
+        if ($mysqli->connect_error) {
+            die('Error: '.$mysqli->connect_error);
+        }
+
+        return $mysqli;
+    }
+
+    function listLanguages() {
+        $mysqli = initConnectionDb();
+        $languageList = $mysqli->query(query: "SELECT * FROM languages");
+
+        $languageObjectArray = [];
+        foreach ($languageList as $languageItem) {
+            $languageObject = new Language($languageItem['id'], $languageItem['name'], $languageItem['iso_code']);
+            array_push($languageObjectArray, $languageObject);
+        }
+        $mysqli->close();
+
+        return $languageObjectArray;
+    }
+
+    function storeLanguage($languageName, $languageISO) {
+        $mysqli = initConnectionDb();
+
+        $languageCreated = false;
+        //TODO: comprobar que no exista un idioma con el mismo ISO code.
+        if ($resultadoInsert = $mysqli->query(query: "INSERT INTO languages (name, iso_code) values ('$languageName', '$languageISO')")) {
+            $languageCreated = true;
+        }   
+        $mysqli->close();
+
+        return $languageCreated;
+    }
+
+    function updateLanguage($languageId, $languageName, $languageISO) {
+        $mysqli = initConnectionDb();
+
+        $languageEdited = false;
+
+        if ($resultadoUpdate = $mysqli->query(query: "UPDATE languages set name = '$languageName', iso_code = '$languageISO' where id = $languageId")) {
+            $languageEdited = true;
+        }
+
+        $mysqli->close();
+
+        return $languageEdited;
+    }
+
+    function getLanguageData($idLanguage) {
+        $mysqli = initConnectionDb();
+        $languageData = $mysqli->query(query: "SELECT * FROM languages WHERE id=$idLanguage");
+
+        $languageObject = null;
+        foreach ($languageData as $languageItem) {
+            $languageObject = new Language($languageItem['id'], $languageItem['name'], $languageItem['iso_code']);
+            break;
+        }
+        $mysqli->close();
+
+        return $languageObject;
+    }
+
+    function deleteLanguage($languageId) {
+        $mysqli = initConnectionDb();
+
+        $languageDeleted = false;
+
+        if ($resultado = $mysqli->query(query: "DELETE FROM languages where id = $languageId")) {
+            $languageDeleted = true;
+        }
+
+        $mysqli->close();
+
+        return $languageDeleted;
+    }
+?>
